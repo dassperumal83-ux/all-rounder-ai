ed080c041ba14c71c767b1bd331aae8183700d47
dassperumal83-ux <dassperumal83@gmail.com>
2026-06-20 22:32:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ def chat():
         return "API Key not set yet bro!"
         
 if__name__=="__main__":
-    port=int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
